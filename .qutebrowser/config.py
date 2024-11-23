40039fd47b8641bb5bb6c99b1a8fb340b1bb99bb
lulululaulau <ma.exp.lau@gmail.com>
2024-11-22 20:33:51 -0500

    initial qutebrowser commit

---
+++ .qutebrowser/config.py
@@ -0,0 +1,13 @@
+config.load_autoconfig()
+c.tabs.position="right"
+c.tabs.show="always"
+c.confirm_quit=["downloads"]
+c.downloads.position="bottom"
+c.tabs.width="12%"
+c.url.searchengines={"DEFAULT":"https://google.com/search?q={}"}
+c.url.default_page="https://google.com/"
+c.tabs.last_close="close"
+c.tabs.select_on_remove="last-used"
+c.tabs.title.format="{audio}{relative_index}: {current_title}"
+c.tabs.new_position.unrelated="next"
+c.editor.command=["/opt/homebrew/bin/vimr", "--line", "{line}", "{file}"]
