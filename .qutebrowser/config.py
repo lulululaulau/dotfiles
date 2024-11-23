3551b9db08929650001f6db9664e95ae738c8796
lulululaulau <ma.exp.lau@gmail.com>
2024-11-23 17:08:50 -0500

    changed window title and tab close behavior

--- .qutebrowser/config.py
+++ .qutebrowser/config.py
@@ -7,7 +7,8 @@ c.tabs.width="12%"
 c.url.searchengines={"DEFAULT":"https://google.com/search?q={}"}
 c.url.default_page="https://google.com/"
 c.tabs.last_close="close"
-c.tabs.select_on_remove="last-used"
+c.tabs.select_on_remove="prev"
 c.tabs.title.format="{audio}{relative_index}: {current_title}"
 c.tabs.new_position.unrelated="next"
 c.editor.command=["/opt/homebrew/bin/vimr", "--line", "{line}", "{file}"]
+c.window.title_format="{audio}{host} {current_title}"
