544ed2113e1720716c6c0cc54bc4e4c6d2600d38
lulululaulau <ma.exp.lau@gmail.com>
2025-03-19 13:25:20 -0400

    attempted to add youtube adblock to qutebrowser

--- .qutebrowser/config.py
+++ .qutebrowser/config.py
@@ -1,8 +1,11 @@
+from qutebrowser.api import interceptor
+
 config.load_autoconfig()
 c.tabs.position="right"
 c.tabs.show="always"
 c.confirm_quit=["downloads"]
 c.content.pdfjs=True
+c.content.blocking.method="both"
 c.downloads.position="bottom"
 c.downloads.remove_finished=60000
 c.downloads.location.remember=False
@@ -16,3 +19,17 @@ c.tabs.new_position.unrelated="next"
 c.editor.command=["/opt/homebrew/bin/vimr", "--line", "{line}", "{file}"]
 c.window.title_format="{audio}{host} {current_title}"
 c.bindings.commands={"normal": {"I": "mode-enter passthrough"}}
+
+
+def filter_youtube(info: interceptor.Request):
+    """Block given request if necessary"""
+    url = info.request_url
+    if (
+        url.host() == "www.youtube.com"
+        and url.path() == "/get_video_info"
+        and "&adformat=" in url.query()
+    ):
+        info.block()
+
+
+interceptor.register(filter_youtube)
