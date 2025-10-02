f268819fe3e586fc55c822f138e9eb4d84425237
lulululaulau <ma.exp.lau@gmail.com>
2025-10-02 11:02:22 -0400

    sketchybar calendar: refactor 0/pos casework

--- .config/sketchybar/plugins/calendar.sh
+++ .config/sketchybar/plugins/calendar.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+eventnumber=${1}
+if [ ${1} -eq 0 ]; then
+  eventnumber=1
+fi
+
 # check internet connection before calling fiveevents.py
 if [ ${1} -eq 0 ]; then
   if ( system_profiler SPAirPortDataType | grep -q "Status: Connected" ) ; then
@@ -7,35 +12,14 @@ if [ ${1} -eq 0 ]; then
   fi
   sketchybar --trigger calendarLoaded
 fi
-if [ ${1} -gt 0 ]; then
-  event=$(sed -n "${1}p" ~/.config/sketchybar/plugins/calendarEvents)
-else
-  event=$(sed -n "1p" ~/.config/sketchybar/plugins/calendarEvents)
-fi
+event=$(sed -n "${eventnumber}p" ~/.config/sketchybar/plugins/calendarEvents)
 
 numEvents=$(wc -l ~/.config/sketchybar/plugins/calendarEvents | cut -c8-8)
-if [ ${1} -gt ${numEvents} ]; then
-  if [ ${1} -gt 1 ]; then
+if [ ${eventnumber} -gt ${numEvents} ]; then
+  if [ ${eventnumber} -gt 1 ]; then
     LABEL="No More Events"
   else
     LABEL="No Events"
-  fi
-  if [ ${1} -eq 0 ]; then
-    if [ ${numEvents} -gt 0 ]; then
-      if ( echo ${event} | cut -c11- | grep -q -e "^T" ) ; then
-        datetime=$(gdate -d "$(echo ${event} | head -c 25)" +"%b %-d %-H:%M")
-        summary=$(echo ${event} | cut -c27-)
-      else
-        datetime=$(gdate -d "$(echo ${event} | head -c 10)" +"%b %-d")
-        summary=$(echo ${event} | cut -c12-)
-      fi
-      LABEL="${datetime} ${summary}"
-
-
-      if (( $(echo $LABEL | wc -c) > 40 )); then
-        LABEL="$(echo $LABEL | head -c 36) ..."
-      fi
-    fi
   fi
 else
   if ( echo ${event} | cut -c11- | grep -q -e "^T" ) ; then
