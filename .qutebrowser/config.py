9ed932cc3830df85e37cab1eea85f719a2f5e8c6
lulululaulau <ma.exp.lau@gmail.com>
2025-10-08 12:29:19 -0400

    reordered .qutebrowser/config.py

--- .qutebrowser/config.py
+++ .qutebrowser/config.py
@@ -1,29 +1,31 @@
 from qutebrowser.api import interceptor
 
 config.load_autoconfig(False)
-c.tabs.position="right"
-c.tabs.show="multiple"
+c.aliases["ww"] = "session-save --only-active-window"
 c.confirm_quit=["always"]
-c.content.pdfjs=True
 c.content.blocking.method="both"
-c.downloads.position="bottom"
-c.downloads.remove_finished=60000
+c.content.pdfjs=True
 c.downloads.location.remember=False
 c.downloads.location.suggestion="both"
+c.downloads.position="bottom"
+c.downloads.remove_finished=60000
+c.editor.command=["/opt/homebrew/bin/vimr", "--line", "{line}", "{file}"]
+c.tabs.last_close="close"
+c.tabs.new_position.unrelated="next"
+c.tabs.position="right"
+c.tabs.select_on_remove="prev"
+c.tabs.show="multiple"
+c.tabs.title.format="{audio}{relative_index}: {current_title}"
 c.tabs.width="12%"
 c.url.default_page="mail.google.com/mail/u/0/#inbox"
 c.url.searchengines={'DEFAULT': 'https://google.com/search?q={}&udm=14'}
-c.tabs.last_close="close"
-c.tabs.select_on_remove="prev"
-c.tabs.title.format="{audio}{relative_index}: {current_title}"
-c.tabs.new_position.unrelated="next"
-c.editor.command=["/opt/homebrew/bin/vimr", "--line", "{line}", "{file}"]
 c.window.title_format="{audio}{host} {current_title}"
+
 config.bind('I', 'mode-enter passthrough')
-c.aliases["ww"] = "session-save --only-active-window"
 
 config.bind('o', 'cmd-set-text -s :open -t')
 config.bind('O', 'cmd-set-text -s :open')
+
 config.bind('tdh', 'config-cycle -t -u *://{url:host}/* colors.webpage.darkmode.enabled ;; reload')
 config.bind('tdu', 'config-cycle -t -u {url} colors.webpage.darkmode.enabled ;; reload')
 config.bind('tdH', 'config-cycle -t -u *://*.{url.host}/* colors.webpage.darkmode.enabled ;; reload')
