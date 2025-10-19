4cf315e8921a546841c297700a968b06c02029c9
lulululaulau <ma.exp.lau@gmail.com>
2025-10-19 19:11:40 -0400

    nvim: lint on BufRead

--- .config/nvim/lua/linter.lua
+++ .config/nvim/lua/linter.lua
@@ -2,7 +2,7 @@ require('lint').linters_by_ft = {
   tex = {'chktex'},
 }
 
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+vim.api.nvim_create_autocmd({ "BufRead", "BufWritePost" }, {
   callback = function()
     require("lint").try_lint()
   end,
