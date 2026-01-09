a85a4e4f810a256859d2e646e33ec1b6592e5751
Lewis Lau <lewislau121@gmail.com>
2026-01-08 21:03:55 -0500

    nvim: H and L for buffer navigation

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -8,4 +8,6 @@ vim.keymap.set("v", ":", ";")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set("n", "H", ":bp<CR>")
+vim.keymap.set("n", "L", ":bn<CR>")
 
