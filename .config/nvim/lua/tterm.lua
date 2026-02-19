fb867b688ac0f35d8604eceb2d6fc6d5d0ce0264
Lewis Lau <lewislau121@gmail.com>
2026-02-19 14:42:16 -0500

    toggleterm: shortcuts to spec horizontal and float

--- .config/nvim/lua/tterm.lua
+++ .config/nvim/lua/tterm.lua
@@ -2,3 +2,11 @@ require("toggleterm").setup{
   size = 20,
   open_mapping = [[<leader>c]]
 }
+
+vim.keymap.set("n", "<leader>hc", function ()
+  require("toggleterm").toggle(vim.v.count,0,"get_dir","horizontal")
+end)
+
+vim.keymap.set("n", "<leader>fc", function ()
+  require("toggleterm").toggle(vim.v.count,0,"det_dir","float")
+end)
