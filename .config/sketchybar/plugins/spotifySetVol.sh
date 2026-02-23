ee2d70b89919e79d1a357dc5bcd9d051cae8ce16
Lewis Lau <lewislau121@gmail.com>
2026-02-23 12:54:16 -0500

    sketchybar: spotify volume controls

---
+++ .config/sketchybar/plugins/spotifySetVol.sh
@@ -0,0 +1,2 @@
+osascript -e "tell application \"Spotify\" to set sound volume to $PERCENTAGE"
+sketchybar --trigger spotify_vol
---
+++ .config/sketchybar/plugins/spotifyVol.sh
@@ -0,0 +1,1 @@
+sketchybar --set $NAME label="$(osascript -e 'tell application "Spotify" to sound volume as integer')"
