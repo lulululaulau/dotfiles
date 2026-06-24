807172259458b9f6b3879c22c713bace038a9ebf
Lewis Lau <lewislau121@gmail.com>
2026-06-24 09:23:33 +0800

    sketchybar: spotify img + sep spot. & sketchybarrc

---
+++ .config/sketchybar/plugins/spotifyImage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if ( \
+  curl $(osascript -e 'tell application "Spotify" to get artwork url of current track') \
+  --output ~/.config/sketchybar/plugins/image.jpg
+); then
+  echo "image success"
+else
+  cp ~/.config/sketchybar/plugins/defaultImage.jpg ~/.config/sketchybar/plugins/image.jpg
+fi
+
+sketchybar -m --set $NAME background.image="~/.config/sketchybar/plugins/image.jpg"
