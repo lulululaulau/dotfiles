9a104b04bd1803aec3d767e5be849e63f8231d8f
Lewis Lau <lewislau121@gmail.com>
2026-06-24 08:47:51 +0800

    sketchybar: properly hide spotifyVolPopup

--- .config/sketchybar/plugins/spotifyShowHide.sh
+++ .config/sketchybar/plugins/spotifyShowHide.sh
@@ -3,3 +3,20 @@ sketchybar --set spotifyNext drawing=toggle \
   --set spotifyPlayPause drawing=toggle \
   --set spotifyIndicator drawing=toggle \
   --set spotifyVol drawing=toggle
+
+if ( \
+  sketchybar --query spotifyVol | \
+  tail -n +$(sketchybar --query spotifyVol | \
+  grep "\"popup\"" -n | \
+  sed -e "s/:.*//g") | \
+  tail -n +2 | \
+  sed -e ':again' -e N -e '$!b again' -e 's/{[^}]*}//g' | \
+  grep "drawing" | \
+  grep -q -e "on" \
+); then
+  sketchybar --set spotifyVolSlider drawing=off
+  sketchybar --set spotifyVol popup.drawing=off
+else
+  sketchybar --set spotifyVolSlider drawing=on
+fi
+
