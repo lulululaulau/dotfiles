47b26db3f9b84c83fd4a5f491841fd98ad00b7b7
Lewis Lau <lewislau121@gmail.com>
2026-06-25 21:03:06 +0800

    sketchybar: more battery icons

--- .config/sketchybar/plugins/battery.sh
+++ .config/sketchybar/plugins/battery.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 LABEL=$(pmset -g batt | tail -n1 | grep -Eo "\d+%")
 
+# 󰂎 󰁺 󰁻 󰁼 󰁽 󰁾 󰁿 󰂀 󰂁 󰂂 󰁹
 case "$LABEL" in
-  0[0-9]%)
-    ICON="󰂃"
-    ;;
-  [5-9][0-9]%)
-    ICON="󰁹"
-    ;;
-  100%)
-    ICON="󰁹"
-    ;;
-  [1-4][0-9]%)
-    ICON="󰁾"
-    ;;
+  0[0-9]%) ICON="󰂃" ;;
+  1[0-9]%) ICON="󰁺" ;;
+  2[0-9]%) ICON="󰁻" ;;
+  3[0-9]%) ICON="󰁼" ;;
+  4[0-9]%) ICON="󰁽" ;;
+  5[0-9]%) ICON="󰁽" ;;
+  6[0-9]%) ICON="󰁿" ;;
+  7[0-9]%) ICON="󰂀" ;;
+  8[0-9]%) ICON="󰂁" ;;
+  9[0-9]%) ICON="󰂂" ;;
+  100%) ICON="󰁹" ;;
   # *) ICON="󰁹" ;;
   *)
     ICON=󰂃
@@ -24,5 +24,4 @@ esac
 pmset -g batt | grep 'AC Power' > /dev/null && ICON="󰂄"
 
 sketchybar --set $NAME icon="$ICON"
-sketchybar --set $NAME icon.padding_right=5
 sketchybar --set $NAME label="$LABEL"
